7035c89b4bfbd79aba3e054fd43c5ccfce0ae4c1
xiandexiao <zangxiaoxian@qq.com>
2018-04-18 19:43:37 +0800

    修改字段 userName 为 userId

--- bbs/src/main/java/mnu/bbs/domain/entity/Post.java
+++ bbs/src/main/java/mnu/bbs/domain/entity/Post.java
@@ -23,7 +23,7 @@ public class Post extends Super<Post> {
 	/**
 	 * 贴子发布人名
 	 */
-	private String userName;
+	private Integer userId;
 	/**
 	 * 贴子类型
 	 */
@@ -37,6 +37,10 @@ public class Post extends Super<Post> {
 	 */
 	private String content;
 	/**
+	 * 访问量
+	 */
+	private Integer visited;
+	/**
 	 * 创建时间
 	 */
 	@TableField(fill = FieldFill.INSERT)
@@ -47,6 +51,9 @@ public class Post extends Super<Post> {
 	@TableField(fill = FieldFill.INSERT_UPDATE)
 	private Date updateTime;
 	
+	public Post () {
+	}
+	
 	public String getTitle () {
 		return title;
 	}
@@ -55,12 +62,12 @@ public class Post extends Super<Post> {
 		this.title = title;
 	}
 	
-	public String getUserName () {
-		return userName;
+	public Integer getUserId () {
+		return userId;
 	}
 	
-	public void setUserName (String userName) {
-		this.userName = userName;
+	public void setUserId (Integer userId) {
+		this.userId = userId;
 	}
 	
 	public Integer getType () {
@@ -87,6 +94,14 @@ public class Post extends Super<Post> {
 		this.url = url;
 	}
 	
+	public Integer getVisited () {
+		return visited;
+	}
+	
+	public void setVisited (Integer visited) {
+		this.visited = visited;
+	}
+	
 	public Date getCreateTime () {
 		return createTime;
 	}
@@ -107,10 +122,11 @@ public class Post extends Super<Post> {
 	public String toString () {
 		return "Post{" +
 				"title='" + title + '\'' +
-				", userName='" + userName + '\'' +
+				", userId='" + userId + '\'' +
 				", type=" + type +
 				", url='" + url + '\'' +
 				", content='" + content + '\'' +
+				", visited='" + visited + '\'' +
 				", createTime=" + createTime +
 				", updateTime=" + updateTime +
 				'}';
